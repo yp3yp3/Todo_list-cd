b69376a740f0557ae1573d25e4e825df8a436442
yoel <you@example.com>
2025-06-03 17:23:34 +0300

    test terraform

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -8,29 +8,29 @@ backend "s3" {
 }
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "my-terraform-state-bucket985789345"
-
-  tags = {
-    Name        = "Terraform State Bucket"
-  }
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-resource "aws_dynamodb_table" "terraform_state_lock" {
-  name           = "terraform_state_lock"
-  billing_mode   = "PAY_PER_REQUEST"
-  hash_key       = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = {
-    Name = "Terraform State Lock Table"
-  }
-  lifecycle {
-    prevent_destroy = true
-  }
-}
+#resource "aws_s3_bucket" "terraform_state" {
+#  bucket = "my-terraform-state-bucket985789345"
+#
+#  tags = {
+#    Name        = "Terraform State Bucket"
+#  }
+#  lifecycle {
+#    prevent_destroy = true
+#  }
+#}
+#resource "aws_dynamodb_table" "terraform_state_lock" {
+#  name           = "terraform_state_lock"
+#  billing_mode   = "PAY_PER_REQUEST"
+#  hash_key       = "LockID"
+#  attribute {
+#    name = "LockID"
+#    type = "S"
+#  }
+#
+#  tags = {
+#    Name = "Terraform State Lock Table"
+#  }
+#  lifecycle {
+#    prevent_destroy = true
+#  }
+#}
