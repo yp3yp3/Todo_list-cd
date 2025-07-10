9f2b96f49b57a4587f3bcaf16f9941e997972daf
Yoel Fatal <yp3yp3@gmail.com>
2025-07-10 19:00:33 +0300

    add dns tf

---
+++ k8s/terraform/dns.tf
@@ -0,0 +1,16 @@
+data "aws_route53_zone" "mysite" {
+  name         = var.url_site
+}
+data "kubernetes_service" "nginx_ingress_lb" {
+  metadata {
+    name      = "nginx-ingress-controller"
+    namespace = "ingress-nginx"
+  }
+}
+resource "aws_route53_record" "ingress_record" {
+  zone_id = data.aws_route53_zone.mysite.zone_id
+  name    = "*.${var.url_site}" # Replace with your actual domain
+  type    = "CNAME"
+    ttl     = 300
+    records = [data.kubernetes_service.nginx_ingress_lb.status[0].load_balancer[0].ingress[0].hostname]
+    }
--- k8s/terraform/variables.tf
+++ k8s/terraform/variables.tf
@@ -6,3 +6,14 @@ variable "region" {
   type        = string
   default     = "eu-central-1"
 }
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "mycluster"
+}
+variable "url_site" {
+  description = "Domain name"
+  type        = string
+  default     = "yp3yp3.site" # Replace with your actual domain
+  
+}
