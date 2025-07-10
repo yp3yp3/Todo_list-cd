9c1b82cf78912e1ff28b53f714149a44b53917fd
Yoel Fatal <yp3yp3@gmail.com>
2025-07-10 18:03:41 +0300

    add ingress tf

---
+++ k8s/terraform/ingress.tf
@@ -0,0 +1,28 @@
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  
+}
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    token                  = data.aws_eks_cluster_auth.cluster.token
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  }
+}
+resource "helm_release" "nginx_ingress" {
+  name       = "nginx-ingress"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  version    = "5.4.0"
+
+    namespace  = "ingress-nginx"
+    create_namespace = true
+
+  depends_on = [module.eks]
+  
+}
