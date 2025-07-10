032c162fc23276f744c0bd238b0be0cdd5f7da73
Yoel Fatal <yp3yp3@gmail.com>
2025-07-10 18:07:26 +0300

    add ingress tf

--- k8s/terraform/ingress.tf
+++ k8s/terraform/ingress.tf
@@ -1,5 +1,5 @@
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
