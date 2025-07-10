d2a7c4a09f6360637a4f728215fe254c750a832a
Yoel Fatal <yp3yp3@gmail.com>
2025-07-10 19:27:23 +0300

    add dns tf

--- k8s/terraform/argocd.tf
+++ k8s/terraform/argocd.tf
@@ -27,3 +27,14 @@ resource "helm_release" "argocd" {
   ]
   depends_on = [module.eks]
 }
+data "kubernetes_secret" "argocd_admin_password" {
+  metadata {
+    name      = "argocd-initial-admin-secret"
+    namespace = "argocd"
+  }
+}
+output "argocd_admin_password" {
+  value     = data.kubernetes_secret.argocd_admin_password.data["password"]
+  sensitive = true
+  
+}
