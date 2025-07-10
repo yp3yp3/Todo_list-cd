821ab5f2292f3d4b5ce6a1962ef08ee23502c9e9
Yoel Fatal <yp3yp3@gmail.com>
2025-07-10 18:06:13 +0300

    add ingress tf

--- k8s/terraform/ingress.tf
+++ k8s/terraform/ingress.tf
@@ -8,7 +8,7 @@ provider "kubernetes" {
   
 }
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = module.eks.cluster_endpoint
     token                  = data.aws_eks_cluster_auth.cluster.token
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
