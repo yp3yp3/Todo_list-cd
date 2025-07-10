3d2b6c5d654dc3c04cff28047c947575dd059c23
Yoel Fatal <yp3yp3@gmail.com>
2025-07-10 18:26:39 +0300

    add argocd tf

--- k8s/terraform/argocd.tf
+++ k8s/terraform/argocd.tf
@@ -6,19 +6,20 @@ resource "helm_release" "argocd" {
   namespace  = "argocd"
   create_namespace = true
 
-  set {
+  set = [
+    {
     name  = "server.ingress.enabled"
     value = "true"
-  }
-set {
+  },
+  {
     name  = "server.ingress.ingressClassName"
     value = "nginx" # Ensure this matches your ingress controller class
-  }
+  },
 
-  set {
+   {
     name  = "server.ingress.hosts[0]"
     value = "argocd.yp3yp3.site" # Replace with your actual domain
   }
-
+  ]
   depends_on = [module.eks]
 }
