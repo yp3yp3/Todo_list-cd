fe87e058776d9e755dfae74f8b5f37238d079827
Yoel Fatal <yp3yp3@gmail.com>
2025-07-10 18:08:32 +0300

    add ingress tf

--- k8s/terraform/ingress.tf
+++ k8s/terraform/ingress.tf
@@ -18,7 +18,7 @@ resource "helm_release" "nginx_ingress" {
   name       = "nginx-ingress"
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
-  version    = "5.4.0"
+
 
     namespace  = "ingress-nginx"
     create_namespace = true
