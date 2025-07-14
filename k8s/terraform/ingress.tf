4d4eb67e17aad6e5bc1c845d6a7a9053320b1f82
Yoel Fatal <yp3yp3@gmail.com>
2025-07-14 22:26:31 +0300

    fix

--- k8s/terraform/ingress.tf
+++ k8s/terraform/ingress.tf
@@ -5,7 +5,6 @@ provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   token                  = data.aws_eks_cluster_auth.cluster.token
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-  wait_for_kubeconfig    = true
 }
 provider "helm" {
   kubernetes = {
