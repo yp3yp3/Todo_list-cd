72e963c361db5fe1379d8ed09b70944e9a10190f
Yoel Fatal <yp3yp3@gmail.com>
2025-07-14 22:16:57 +0300

    fix

--- k8s/terraform/argocd.tf
+++ k8s/terraform/argocd.tf
@@ -39,8 +39,12 @@ output "argocd_admin_password" {
   sensitive = true
   
 }
+resource "time_sleep" "wait_for_nodes" {
+  depends_on = [data.aws_eks_node_group.default]
+  create_duration = "60s"              
+}
 
 resource "kubernetes_manifest" "root-app" {
   manifest = yamldecode(file("../argo/root-app.yaml"))
-  depends_on = [helm_release.argocd]
+  depends_on = [helm_release.argocd, time_sleep.wait_for_nodes]
 }
